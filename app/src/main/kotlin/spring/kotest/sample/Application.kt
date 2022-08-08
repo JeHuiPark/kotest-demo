fe5c77a2e74744bcd4023234bacdec5179394238
jehuipark <pjh2359@gmail.com>
2022-08-08 23:55:02 +0900

    Implement demo service

--- app/src/main/kotlin/spring/kotest/sample/Application.kt
+++ app/src/main/kotlin/spring/kotest/sample/Application.kt
@@ -1,6 +1,29 @@
 package spring.kotest.sample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class Application
+
+data class DemoModel(
+    val stringProperty: String,
+    val intProperty: Int,
+    val booleanProperty: Boolean,
+)
+
+@Service
+class DemoService(val printService: PrintService) {
+
+    fun execute(demoModel: DemoModel) {
+        printService.print(demoModel)
+    }
+}
+
+@Service
+class PrintService {
+
+    fun print(demoModel: DemoModel) {
+        println("received model = $demoModel")
+    }
+}
