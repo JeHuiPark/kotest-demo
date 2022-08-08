9f1c9587740113e10cc5b0ca7f6424832151d720
jehuipark <pjh2359@gmail.com>
2022-08-09 00:20:16 +0900

    Add fully pojo test

---
+++ app/src/test/kotlin/spring/kotest/sample/DemoServiceFullyPojoTest.kt
@@ -0,0 +1,40 @@
+package spring.kotest.sample
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.CapturingSlot
+import io.mockk.mockk
+import io.mockk.verify
+
+class DemoServiceFullyPojoTest : BehaviorSpec({
+
+    val printService = mockk<PrintService>(
+        relaxed = true // https://mockk.io/#relaxed-mock
+    )
+    val demoService = DemoService(printService)
+
+    // simple behavior test
+    given("a demoModel") {
+        val given = DemoModel(
+            stringProperty = "str",
+            intProperty = 1,
+            booleanProperty = true,
+        )
+        `when`("always") {
+            demoService.execute(given)
+            then("request to PrintService to print demoModel - verify with only behavior") {
+                verify(exactly = 1) { printService.print(any()) }
+            }
+
+            then("request to PrintService to print demoModel - verify with argument match") {
+                verify(exactly = 1) { printService.print(eq(given)) }
+            }
+
+            then("request to PrintService to print demoModel - verify with argument capture") {
+                val captureSlot = CapturingSlot<DemoModel>()
+                verify(exactly = 1) { printService.print(capture(captureSlot)) }
+                captureSlot.captured shouldBe given
+            }
+        }
+    }
+})
